920049c880056b6411a37355649e0207c4418d6b
ThalisonCosta202502608691 <202502608691@alunos.estacio.br>
2025-02-13 14:57:45 +0000

    comentários adicionados para uso futuro.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+/*OBSERVAÇÕES DO ALUNO: 
+-Solicitações de etapas do curso(Novato,Aventureiro,Mestre) são muito confusas e aparentam desorganização
+-Necessário revisão para demonstrar ao aluno corretamente o que é necessário em cada etapa do código.
+-Solicitações atuais não correspondem ao nível de conhecimento apresentado nas aulas e criam frustração no aluno que não possui
+informações sobre vetores, arrays, limpeza de buffer e chamada de biblioteca padrão.
+-VSCode não é tão "beginner friendly"(interface suja demais) como CODEBLOCKS
+ e apresenta diversos erros de instalação dada versão atual e demonstração datada anteriormente(?) para aula do curso.
+
+INFORMAÇÕES DESEJADAS:
+ -Número de países com os quais o programa trabalhará
+ -Onde começar e onde parar o código para que tempo de estudo tenha delimitação
+ -Limpeza de buffer de gets teria ajudado bastante se ensinado no nivel NOVATO.
+ -Vetores para utilizar de comando 'for(i = 0;i > scanf(variavel),i++)' para criar multiplas cartas com menos variaveis e linhas de codigo.
+ -Arrays para organização de subtipos com 'for' mencionado acima
+
+*/
+
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste larissa(quem é larissa?)
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
