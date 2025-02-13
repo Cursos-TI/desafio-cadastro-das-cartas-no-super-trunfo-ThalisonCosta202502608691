9d4999a7a9a0bd6219767c52d137f828e9765b66
ThalisonCosta202502608691 <202502608691@alunos.estacio.br>
2025-02-13 14:45:07 +0000

    debug/teste correção de entradas e saídas padrão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,33 +24,45 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     printf("Digite o nome do País: "); //todos os printf demonstram saida no console para solicitar proximo comando
-    scanf("%s ", &pais); //todos os scanf atribuem comandos as solicitações anteriores dos printf
-    printf("\nDigite o codigo(numeral de 1-4) da cidade: ");
-    scanf("%d ", &codigoCidade);
-    printf("\nDigite o codigo(letra A-Z)da cidade: ");
-    scanf("%c ", &codigoCidadeLetra);
-    printf("\nDigite o nome da cidade: ");
-    scanf("%s ", &nomeCidade);
-    printf("\nDigite a populacao da cidade: ");
-    scanf("%lf ", &populacaoCidade);
-    printf("\nDigite a area da cidade: ");
-    scanf("%lf ", &areaCidade);
-    printf("\nDigite o PIB da cidade: ");
-    scanf("%lf ", &pibCidade);
-    printf("\nDigite a quantidade de pontos turisticos da cidade: ");
-    scanf("%d ", &turisticoCidade);
+     scanf("%s", &pais); //todos os scanf atribuem comandos as solicitações anteriores dos printf
+    printf("Digite o codigo(numeral de 1-4) da cidade: ");
+     scanf("%d", &codigoCidade);
+    printf("Digite o codigo(letra A-Z)da cidade: ");
+     scanf(" %c", &codigoCidadeLetra);
+    printf("Digite o nome da cidade: ");
+     scanf("%s", &nomeCidade);
+    printf("Digite a populacao da cidade: ");
+     scanf("%lf", &populacaoCidade);
+    printf("Digite a area da cidade: ");
+     scanf("%lf", &areaCidade);
+    printf("Digite o PIB da cidade: ");
+     scanf("%lf", &pibCidade);
+    printf("Digite a quantidade de pontos turisticos da cidade: ");
+     scanf("%d", &turisticoCidade);
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Pais: %s", pais);
-    printf("Codigo da cidade: %d%c", codigoCidade, codigoCidadeLetra);
-    printf("Cidade: %s", nomeCidade);
-    printf("Populacao: %lf", populacaoCidade);
-    printf("Area(Km²): %lf", areaCidade);
-    printf("PIB: %lf", pibCidade);
-    printf("Pontos Turisticos: %d", turisticoCidade);
+    printf("\n");
+    printf("Pais: %s\n", pais);
+    printf("Codigo da cidade: %d%c\n", codigoCidade, codigoCidadeLetra);
+    printf("Cidade: %s\n", nomeCidade);
+    printf("Populacao: %.0lf\n", populacaoCidade);
+    printf("Area: %.0lf(Km²)\n", areaCidade);
+    printf("PIB: %.1lf(Bi R$)\n", pibCidade);
+    printf("Pontos Turisticos: %d\n", turisticoCidade);
+
+    /*dados para preenchimento de teste/debug
+    Brasil
+    1
+    B
+    Brasilia
+    2.817.000 populacao
+    5.760.784 km² area
+    328.800.000.000 pib
+    32 turistico 
+    */
 
     return 0;
 }
